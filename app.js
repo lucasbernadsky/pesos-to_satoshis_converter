7b994c69c5b3455cc6f1436fb20cca738b55dfe6
Joaquin Naftaly <32153618+joaconafta@users.noreply.github.com>
2024-01-29 01:06:44 -0300

    api calls and refactoring

--- app.js
+++ app.js
@@ -1,45 +1,45 @@
-function convertir() {
+async function convertir() {
     const monto = parseFloat(document.getElementById('amount').value);
     const conversionType = document.getElementById('conversionType').value;
+    const cotizacionSatoshi = await obtenerCotizacionSatoshi()
+    const cotizacionDolarBlue = await obtenerCotizacionDolarBlue()
 
     if (conversionType === 'arsToSatoshis') {
-        convertirPesosASatoshis(monto);
+        convertirPesosASatoshis(monto, cotizacionSatoshi, cotizacionDolarBlue);
     } else if (conversionType === 'satoshisToARS') {
-        convertirSatoshisAPesos(monto);
+        convertirSatoshisAPesos(monto, cotizacionSatoshi, cotizacionDolarBlue);
     } else {
         console.error('Tipo de conversión no válido');
     }
 }
 
-function convertirPesosASatoshis(montoARS) {
-    // Obtener cotización del Peso Argentino desde una API
-    fetch('URL_DE_LA_API_ARS')
-        .then(response => response.json())
-        .then(data => {
-            const cotizacionARS = data.rates.USD; // Asumiendo que la API devuelve en USD
-            const cotizacionBitcoin = obtenerCotizacionBitcoin(); // Implementar función
-
-            const satoshis = montoARS / (cotizacionARS * cotizacionBitcoin);
-            document.getElementById('result').innerText = `Resultado: ${satoshis} Satoshis`;
-        })
-        .catch(error => console.error('Error al obtener cotización ARS', error));
+function convertirPesosASatoshis(montoARS, cotizacionSatoshi, cotizacionDolarBlue) {
+    const satoshis = (montoARS / cotizacionDolarBlue) / cotizacionSatoshi;
+    document.getElementById('result').innerText = `Resultado: ${satoshis} Satoshis`;
 }
 
-function convertirSatoshisAPesos(montoSatoshis) {
-    // Obtener cotización del Peso Argentino desde una API
-    fetch('URL_DE_LA_API_ARS')
-        .then(response => response.json())
-        .then(data => {
-            const cotizacionARS = data.rates.USD; // Asumiendo que la API devuelve en USD
-            const cotizacionBitcoin = obtenerCotizacionBitcoin(); // Implementar función
-
-            const montoARS = montoSatoshis * cotizacionARS * cotizacionBitcoin;
-            document.getElementById('result').innerText = `Resultado: ${montoARS} Pesos`;
-        })
-        .catch(error => console.error('Error al obtener cotización ARS', error));
+function convertirSatoshisAPesos(montoSatoshis, cotizacionSatoshi, cotizacionDolarBlue) {
+    const montoARS = montoSatoshis * cotizacionDolarBlue * cotizacionSatoshi;
+    document.getElementById('result').innerText = `Resultado: ${montoARS} Pesos`;
 }
 
-function obtenerCotizacionBitcoin() {
+async function obtenerCotizacionSatoshi() {
     // Obtener cotización de Bitcoin desde otra API
     // Implementar la lógica de obtención de la cotización
+    return fetch('https://api.coinbase.com/v2/prices/BTC-USD/buy')
+        .then(response => response.json())
+        .then(data => {
+            return data.data.amount / 100000000            
+        })
+        .catch(error => console.error('Error al obtener cotización BTC', error));
+}
+async function obtenerCotizacionDolarBlue() {
+    // Obtener cotización de Bitcoin desde otra API
+    // Implementar la lógica de obtención de la cotización
+    return fetch('https://api.bluelytics.com.ar/v2/latest')
+        .then(response => response.json())
+        .then(data => {
+            return data.blue.value_sell
+        })
+        .catch(error => console.error('Error al obtener cotización USD', error));
 }
